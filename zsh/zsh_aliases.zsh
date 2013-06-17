5e034366bdf528f8bcc92ffa396af13ceedeede8
shayan <dochead+github@gmail.com>
2013-06-17 17:49:39 +0200

    Added open finder here & edit aliases

--- zsh/zsh_aliases.zsh
+++ zsh/zsh_aliases.zsh
@@ -1,2 +1,4 @@
 alias logonhp='ssh -A shayan@192.168.0.10'
 alias killtunnels='ps ax | grep -i '"'"'ssh -f'"'"' | grep -v grep | awk '"'"'{ print $1 }'"'"' | xargs kill'
+alias edal='edit ~/.dotfiles/zsh/zsh_aliases.zsh'
+alias ofh='open .'   # Open finder here
