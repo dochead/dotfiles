f4275472ad0d162afe53978c218d371c6585c081
shayan <dochead+github@gmail.com>
2013-07-03 07:39:33 +0200

    Added Laduma dev PHP server to aliases

--- zsh/zsh_aliases.zsh
+++ zsh/zsh_aliases.zsh
@@ -1,5 +1,6 @@
 alias logonhp='ssh -A shayan@192.168.0.10'
 alias logonlad='ssh -A www.laduma.co'
+alias logonphp='ssh -A 162.209.53.137'
 
 alias killtunnels='ps ax | grep -i '"'"'ssh -f'"'"' | grep -v grep | awk '"'"'{ print $1 }'"'"' | xargs kill'
 alias tunphp='ssh -f 162.209.53.137 -L 3307:localhost:3306 -N'
