3bd989e5f31362e46864081e550fdb50e26e560f
shayan <dochead+github@gmail.com>
2013-07-03 07:38:19 +0200

    Added Laduma dev server to aliases

--- zsh/zsh_aliases.zsh
+++ zsh/zsh_aliases.zsh
@@ -1,4 +1,10 @@
 alias logonhp='ssh -A shayan@192.168.0.10'
+alias logonlad='ssh -A www.laduma.co'
+
 alias killtunnels='ps ax | grep -i '"'"'ssh -f'"'"' | grep -v grep | awk '"'"'{ print $1 }'"'"' | xargs kill'
+alias tunphp='ssh -f 162.209.53.137 -L 3307:localhost:3306 -N'
+
 alias edal='edit ~/.dotfiles/zsh/zsh_aliases.zsh'
 alias ofh='open .'   # Open finder here
+
+alias gcd='git checkout development'
